d80ff5738a0e39b9c743d83efd3666ee551bc050
Victor de Aguiar <victor.aguiar90@gmail.com>
2020-07-29 00:28:58 -0300

    Added the function to read data

--- python_for_finance.py
+++ python_for_finance.py
@@ -6,8 +6,18 @@ import pandas_datareader.data as web
 
 style.use('ggplot')
 
-start = dt.datetime(2000,1,1)
-end = dt.datetime.now()
+#start = dt.datetime(2000,1,1)
+#end = dt.datetime.now()
 
-df = web.DataReader('RLOG3.SA','yahoo', start, end)
-df.to_csv('RLOG.csv')
+#df = web.DataReader('RLOG3.SA','yahoo', start, end)
+#df.to_csv('RLOG.csv')
+
+#Function used to read csv files
+#Using the option to parse dates
+#Selecting the first column as the index
+df = pd.read_csv('RLOG.csv', parse_dates = True, index_col = 0)
+
+#Used to plot a graph with the selected column and to shw to us
+df['Adj Close'].plot()
+plt.show()
+#print(df.head())
